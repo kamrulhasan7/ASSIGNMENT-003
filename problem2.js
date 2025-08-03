c6b378f73f3d94986ace96bb616113d97aab9092
kamrulmiazi <kamrul116522@gmail.com>
2025-08-03 20:04:01 +0600

    problem 2 compleate

--- problem2.js
+++ problem2.js
@@ -1,10 +1,3 @@
-/** Problem -01 ( Divide the Asset ) */
-var area = 800;
-//write your code here
-var divide = area / 2;
-console.log(divide);
-
-
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
 //write your code here
