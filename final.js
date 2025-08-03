5c3222be37d1f376ee69ef9f16e0fe067e88d8a5
kamrulmiazi <kamrul116522@gmail.com>
2025-08-03 23:48:38 +0600

    problem 5 solved

--- final.js
+++ final.js
@@ -45,3 +45,10 @@ if (
 else {
   console.log("Delete");
 }
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = student.name + student.roll + '.' + student.department + '@ph.ac.bd';
+console.log(email);
